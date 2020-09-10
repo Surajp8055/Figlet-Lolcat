bef2bb981854fbb4eeaa5fec0077cbef1dec3431
Surajp8055 <70626142+Surajp8055@users.noreply.github.com>
2020-09-10 13:39:36 +0530

    Add files via upload

---
+++ danteinstall.sh
@@ -0,0 +1,13 @@
+#!bin/bash
+#this line is know as shebang
+#let's start
+figlet Dante Tool Maker | lolcat
+ read -p "Enter your name :> " NAME
+ echo " [1] figlet "
+ echo " [2] lolcat "
+ pkg install figlet
+ gem install lolcat
+ clear
+ echo " Thank You $NAME" | lolcat
+ figlet For Installing Tool | lolcat
+ 
